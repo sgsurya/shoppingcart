10c5e077b194973fa927956aa2b1766f114bb0fa
Iyer <Suryakiran.Iyer@hitachiconsulting.com>
2017-02-16 10:12:45 +0530

    Updated html and route for custom page

---
+++ ckanext/helpers.py
@@ -0,0 +1,9 @@
+import logging
+from pylons import config
+
+from ckan import plugins as p
+from ckan.lib import helpers as h
+
+from  ckan.lib.helpers import json
+
+log = logging.getLogger(__name__)
--- ckanext/shoppingcart/plugin.py
+++ ckanext/shoppingcart/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class ShoppingcartPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +12,13 @@ class ShoppingcartPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'shoppingcart')
+        toolkit.add_public_directory(config_,'theme/public')
+    
+    def after_map(self, map):
+        controller = 'ckanext.shoppingcart.plugin:controller'
+        map.connect('/custom',controller=controller,action='custom_page')
+        return map
+    
+class CustomController(plugins.toolkit.BaseController):
+    def custom_page(self):
+        return plugins.toolkit.render('custom.html')
